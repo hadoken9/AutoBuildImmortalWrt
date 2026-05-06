158761c6926b68b26c87bf061ee50452c7f110ff
hadoken9 <103473307+hadoken9@users.noreply.github.com>
2026-05-06 22:11:56 +0800

    Update package list in build25.sh
    Removed several luci-related packages and added kmod-mtd-rw and wget.

--- mediatek-filogic/build25.sh
+++ mediatek-filogic/build25.sh
@@ -47,15 +47,11 @@ echo "$(date '+%Y-%m-%d %H:%M:%S') - Starting build process..."
 PACKAGES=""
 PACKAGES="$PACKAGES curl luci luci-i18n-base-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-firewall-zh-cn"
-PACKAGES="$PACKAGES luci-theme-argon"
-PACKAGES="$PACKAGES luci-app-argon-config"
-PACKAGES="$PACKAGES luci-i18n-argon-config-zh-cn"
-PACKAGES="$PACKAGES luci-i18n-diskman-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-package-manager-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-ttyd-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-# 文件管理器
-PACKAGES="$PACKAGES luci-i18n-filemanager-zh-cn"
+PACKAGES="$PACKAGES kmod-mtd-rw"
+PACKAGES="$PACKAGES wget"
 
 
 # 第三方软件包 合并
